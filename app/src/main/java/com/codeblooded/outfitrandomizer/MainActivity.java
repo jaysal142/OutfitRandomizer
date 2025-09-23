1976cb53e24e2b21d13442405ca6e88dccd1a3dd
Jayden Salgado <jayden.salgado@ucdenver.edu>
2025-09-23 17:01:40 -0600

    Sample opening logo/ welcome screen:
    Added animations.
    
    Began work on login screen, mainly just UI. (needs code)

--- app/src/main/java/com/codeblooded/outfitrandomizer/MainActivity.java
+++ app/src/main/java/com/codeblooded/outfitrandomizer/MainActivity.java
@@ -1,6 +1,13 @@
 package com.codeblooded.outfitrandomizer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +17,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView brand, tagline;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +35,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.sampleBrandLogo);
+        brand = findViewById(R.id.sampleBrandText);
+        tagline = findViewById(R.id.sampleTaglineText);
+
+
+        image.setAnimation(topAnim);
+        brand.setAnimation(bottomAnim);
+        tagline.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
